d436dd47c5996649b383a6c721aa78644ee84e9d
Abbas Hussain <abbassiddiqui99@gmail.com>
2021-04-23 01:57:08 +0500

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace w3school_c__OOP_Concepts
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
