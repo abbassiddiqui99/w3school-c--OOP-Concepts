6a63c78385264b1e1adc0c3aaa1156ff49723022
Abbas Hussain <abbassiddiqui99@gmail.com>
2021-04-23 02:32:35 +0500

    simple function overloading example for reference.

--- Program.cs
+++ Program.cs
@@ -4,9 +4,18 @@ namespace w3school_c__OOP_Concepts
 {
     class Program
     {
+        static int addition(int x,int y){
+            return x + y;
+        }
+        static double addition(double x,double y){
+            return x + y;
+        }
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            System.Console.WriteLine(addition(1,1));
+            System.Console.WriteLine(addition(1.234,5.234));
+            Console.ReadLine();
         }
     }
 }
