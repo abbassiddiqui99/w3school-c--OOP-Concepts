aa77cd39f5b353d4e032bd89c1d438fd27b66dc6
Abbas Hussain <abbassiddiqui99@gmail.com>
2021-04-23 21:06:24 +0500

    added comment only

--- Program.cs
+++ Program.cs
@@ -9,6 +9,11 @@ namespace w3school_c__OOP_Concepts
             Car car01 = new Car("civic","grey",1995);
             // 'Car.year' is inaccessible due to its protection level
             // System.Console.WriteLine(car01.year);
+
+            // ******************
+            // You Can Access Private Attributes only by GET & SET properties for
+            // reference see w3school
+            // ******************
             car01.getCarDetails();
             Console.ReadLine();
         }
